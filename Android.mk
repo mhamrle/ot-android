ff0773caf179d7b6f0b362148fb1b8720e2b27ae
FellowTraveler <F3llowTraveler@gmail.com>
2013-12-06 04:31:34 -0600

    openssl-android builds static libs as subproject.
    Signed-off-by: FellowTraveler <F3llowTraveler@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,140 +1,28 @@
 LOCAL_PATH:= $(call my-dir)
 
-
-local_src_files := \
-	otapi/OTAPI.cpp \
-	otapi/OTClient.cpp \
-	otapi/OTServerConnection.cpp \
-	otapi/OpenTransactions.cpp \
-	otapi/OTAPI_Basic.cpp \
-	otapi/OTMadeEasy.cpp \
-	otapi/OT_ME.cpp \
-	otlib/OTASCIIArmor.cpp \
-	otlib/OTAccount.cpp \
-	otlib/OTAgreement.cpp \
-	otlib/OTAssetContract.cpp \
-	otlib/OTAsymmetricKey.cpp \
-	otlib/OTBasket.cpp \
-	otlib/OTBylaw.cpp \
-	otlib/OTCachedKey.cpp \
-	otlib/OTCheque.cpp \
-	otlib/OTContract.cpp \
-	otlib/OTCredential.cpp \
-	otlib/OTCron.cpp \
-	otlib/OTCronItem.cpp \
-	otlib/OTCrypto.cpp \
-	otlib/OTData.cpp \
-	otlib/OTDataCheck.cpp \
-	otlib/OTDigitalCash.cpp \
-	otlib/OTEnvelope.cpp \
-	otlib/OTIdentifier.cpp \
-	otlib/OTInstrument.cpp \
-	otlib/OTItem.cpp \
-	otlib/OTKeyring.cpp \
-	otlib/OTLedger.cpp \
-	otlib/OTLog.cpp \
-	otlib/OTMarket.cpp \
-	otlib/OTMessage.cpp \
-	otlib/OTMessageBuffer.cpp \
-	otlib/OTMint.cpp \
-	otlib/OTOffer.cpp \
-	otlib/OTPassword.cpp \
-	otlib/OTPaths.cpp \
-	otlib/OTPayload.cpp \
-	otlib/OTPayment.cpp \
-	otlib/OTPaymentPlan.cpp \
-	otlib/OTPseudonym.cpp \
-	otlib/OTPurse.cpp \
-	otlib/OTScript.cpp \
-	otlib/OTScriptable.cpp \
-	otlib/OTServerContract.cpp \
-	otlib/OTSettings.cpp \
-	otlib/OTSignature.cpp \
-	otlib/OTSignedFile.cpp \
-	otlib/OTSmartContract.cpp \
-	otlib/OTStorage.cpp \
-	otlib/OTString.cpp \
-	otlib/OTStringXML.cpp \
-	otlib/OTSymmetricKey.cpp \
-	otlib/OTToken.cpp \
-	otlib/OTTrackable.cpp \
-	otlib/OTTrade.cpp \
-	otlib/OTTransaction.cpp \
-	otlib/OTTransactionType.cpp \
-	otlib/OTWallet.cpp \
-	otlib/bigint/BigInteger.cc \
-	otlib/bigint/BigIntegerAlgorithms.cc \
-	otlib/bigint/BigIntegerUtils.cc \
-	otlib/bigint/BigUnsigned.cc \
-	otlib/bigint/BigUnsignedInABase.cc \
-	otlib/irrxml/irrXML.cpp \
-	otlib/lucre/bankimp.cpp \
-	otlib/otext/Timer.cpp \
-	otlib/otext/anyoption.cpp \
-	otlib/otext/tinythread.cpp \
-	otlib/otprotob/Bitcoin.cc \
-	otlib/otprotob/Generics.cc \
-	otlib/otprotob/Markets.cc \
-	otlib/otprotob/Moneychanger.cc \
-	otlib/simpleini/ConvertUTF.cpp \
-	otlib/otext/easyzlib.c \
-	otlib/otext/mkcert.c \
-	../swig/otapi/OTAPI-java.cxx
-
-
-local_c_includes := \
-	$(NDK_PROJECT_PATH) \
-	$(NDK_PROJECT_PATH)/include \
-	$(NDK_PROJECT_PATH)/include/bigint \
-	$(NDK_PROJECT_PATH)/include/containers \
-	$(NDK_PROJECT_PATH)/include/irrxml \
-	$(NDK_PROJECT_PATH)/include/lucre \
-	$(NDK_PROJECT_PATH)/include/otapi \
-	$(NDK_PROJECT_PATH)/include/otlib \
-	$(NDK_PROJECT_PATH)/include/ots \
-	$(NDK_PROJECT_PATH)/include/simpleini \
-	$(NDK_PROJECT_PATH)/swig/otapi
-
-
-local_ld_libs := -lprotobuf -D_THREAD_SAFE -lzmq -lmsgpack -lssl -lcrypto
-
-
-#######################################
-
-# target
+# Clear variables here.
 include $(CLEAR_VARS)
-include $(LOCAL_PATH)/../android-config.mk
-LOCAL_SRC_FILES += $(local_src_files)
-LOCAL_C_INCLUDES += $(local_c_includes)
-LOCAL_LDLIBS += $(local_ld_libs)
-ifeq ($(TARGET_SIMULATOR),true)
-    LOCAL_LDLIBS += -ldl
-endif
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE:= libotapi
-include $(BUILD_SHARED_LIBRARY)
 
-#######################################
-# host shared library
-ifeq ($(WITH_HOST_DALVIK),true)
-    include $(CLEAR_VARS)
-    include $(LOCAL_PATH)/../android-config.mk
-    LOCAL_SRC_FILES += $(local_src_files)
-    LOCAL_C_INCLUDES += $(local_c_includes)
-    LOCAL_LDLIBS += $(local_ld_libs)
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE:= libotapi
-    include $(BUILD_SHARED_LIBRARY)
-endif
+# Current module settings.
+LOCAL_MODULE := native
 
-########################################
-# host static library, which is used by some SDK tools.
+# setup some source files
+#LOCAL_SRC_FILES := file1.c file2.c
 
-include $(CLEAR_VARS)
-include $(LOCAL_PATH)/../android-config.mk
-LOCAL_SRC_FILES += $(local_src_files)
-LOCAL_C_INCLUDES += $(local_c_includes)
-LOCAL_LDLIBS += $(local_ld_libs)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE:= libotapi_static
+# setup some includes
+#LOCAL_C_INCLUDES := $(LOCAL_PATH)/libos/include
+
+# setup the included libs for the main module
+LOCAL_STATIC_LIBRARIES := libcrypto_static libssl_static # note that order matters here
+
 include $(BUILD_STATIC_LIBRARY)
+
+# Include makefiles here. Its important that these 
+# includes are done after the main module, explanation below.
+
+# create a temp variable with the current path, because it 
+# changes after each include
+ZPATH := $(LOCAL_PATH)
+
+include $(ZPATH)/openssl-android/crypto/Android.mk
+include $(ZPATH)/openssl-android/ssl/Android.mk
---
+++ jni/Application.mk
@@ -0,0 +1,3 @@
+APP_PROJECT_PATH := $(shell pwd)
+APP_MODULES = libcrypto_static libssl_static
+APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
